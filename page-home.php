6c68dd2d9176632c5ec0584049c6fa5d03bd97f2
Avraxas <avraxas@gmail.com>
2018-11-07 21:06:01 -0500

    fonts

--- page-home.php
+++ page-home.php
@@ -15,7 +15,7 @@ get_header();?>
 
 <div class="videoWrapper">
 	<div class="overlay"></div>
-    <iframe width="auto" height="auto" src="https://www.youtube.com/embed/<?php the_field('videohome') ?>?autoplay=1&rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0&loop=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="auto" height="auto" src="https://www.youtube.com/embed/<?php the_field('videohome') ?>?autoplay=1&rel=0&modestbranding=0&autohide=0&showinfo=0&controls=0&loop=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <!-- <iframe src="https://player.vimeo.com/video/156270370?background=1&autoplay=1&loop=1&byline=0&title=0"
            frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
 
@@ -120,7 +120,7 @@ get_header();?>
 
 <!-- Impacto -->
 <section class="impacto">
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-12">
 				<h3>GENERAMOS IMPACTOS POSITIVOS</h3>
