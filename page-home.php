8e7c797a8b7edc41bc6e8331748fa679a7d92a86
Avraxas <avraxas@gmail.com>
2018-11-28 14:21:21 -0500

    seccion 4

--- page-home.php
+++ page-home.php
@@ -139,7 +139,7 @@ get_header();?>
 		</div>
 		<div class="row mt-5">
 			<div class="row">
-			<div class="nav flex-column nav-pills col-lg-3 offset-lg-1" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+			<div class="nav flex-column nav-pills col-lg-3 offset-lg-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 			  <a class="nav-link active tt1" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
 			  	<span class="wrap-icon"><i class="fas fa-map-marker-alt tab-icon"></i></span><span class="tab-text"><b>Localización</b></span></a>
 			  <a class="nav-link tt2" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><span class="wrap-icon"><i class="fas fa-search tab-icon"></i></span><span class="tab-text"><b>Análisis</b></span></a>
@@ -147,83 +147,132 @@ get_header();?>
 			  <a class="nav-link tt4" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false"><span class="wrap-icon"><i class="fas fa-file-signature tab-icon"></i></span><span class="tab-text"><b>Gestión</b></span></a>
 			   <a class="nav-link tt5" id="v-pills-settings2-tab" data-toggle="pill" href="#v-pills-settings2" role="tab" aria-controls="v-pills-settings2" aria-selected="false"><span class="wrap-icon"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-urban-value-tab.png" class="tab-icon"></span><span class="tab-text"><b>Retornos</b></span></a>
 			</div>
-			<div class="tab-content col-lg-7 circle" id="v-pills-tabContent">
-			  <div class="tab-pane fade show active text-center" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><p>Descripcion1: Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			<div class="tab-content col-lg-5 circle d-lg-flex h-100" id="v-pills-tabContent">
+			  <div class="tab-pane fade show active text-center justify-content-lg-center align-self-lg-center text-center" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><p>A través del análisis de Big Data se definen las ubicaciones que cumplan los requisitos de inversión de u&v. Reforzamos nuestra propuesta de valor a traves herramientas de Big Data.</p>
 
-				<p>Detalles: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
-
-				<p>Partner o Tecnologia: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-
-				<p>Con la aportacion de: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-
-				<p>Tecnologias y Partners que validan:</p>
-				<div class="text-center"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"></div>
+				<div class="text-center"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
 			</div>
-			  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"><p>Descripcion2: Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			  <div class="tab-pane fade justify-content-lg-center align-self-lg-center text-center" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"><p>Proceso exhaustivo de análisis comercial, financiero y técnico. A través de modelos de Machine Learning se procesan millones de datos tanto internos como externos que permiten fácilmente identificar los insights más valiosos.</p>
 
-				<p>Detalles: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
+				
+				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div></div>
+			  <div class="tab-pane fade justify-content-lg-center align-self-lg-center text-center" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"><p>Captación de fondos para financiar la adquisición y desarrollos de los proyectos. La estructura buscada es la que combine el capital privado / institucional y financiación bancaria. u&v cuenta con una plataforma digital que permite la eficiente gestión de los activos y transparencia para los inversores</p>
 
-				<p>Partner o Tecnologia: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
+				
+				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div></div>
+			  <div class="tab-pane fade justify-content-lg-center align-self-lg-center text-center" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"><p>Estructura de desarrollo óptima a través de contratos de gestión con fees predefinidos. Estándares LEED, proyectos en base BIM y monitorización a través de la plataforma de inversión.</p>
 
-				<p>Con la aportacion de: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div></div>
+			  <div class="tab-pane fade justify-content-lg-center align-self-lg-center text-center" id="v-pills-settings2" role="tabpanel" aria-labelledby="v-pills-settings-tab2"><p>La suma de los elementos anteriormente descritos, resulta en la concesión de los objetivos previamente acordados respecto a la inversión, tanto económicos como de plazo.</p>
 
-				<p>Tecnologias y Partners que validan:</p>
-				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"></div></div>
-			  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"><p>Descripcion3: Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-
-				<p>Detalles: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
-
-				<p>Partner o Tecnologia: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-
-				<p>Con la aportacion de: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-
-				<p>Tecnologias y Partners que validan:</p>
-				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"></div></div>
-			  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"><p>Descripcion4: Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-
-				<p>Detalles: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
-
-				<p>Partner o Tecnologia: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-
-				<p>Con la aportacion de: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-
-				<p>Tecnologias y Partners que validan:</p>
-				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"></div></div>
-			  <div class="tab-pane fade" id="v-pills-settings2" role="tabpanel" aria-labelledby="v-pills-settings-tab2"><p>Descripcion5: Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-
-				<p>Detalles: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
-
-				<p>Partner o Tecnologia: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-
-				<p>Con la aportacion de: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-
-				<p>Tecnologias y Partners que validan:</p>
-				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech"></div></div>
+				
+				<div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div></div>
 			</div>
 		</div>
 		</div>
 	</div>
 </section>
+
+
+<!-- tabbs2 -->
+<section class="tabbs2 d-md-block d-lg-flex h-100 pb-md-5" >
+	<div class="container justify-content-lg-center align-self-lg-center">
+		<div class="row ">
+			<div class="col-lg-12 text-center">
+				<h2 class="mb-3"><b>PROCESO DE INVERSIÓN</b></h2>
+				<h4 class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				</h4>
+				
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="row">
+
+<div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <i class="fas fa-map-marker-alt tab-icon"></i> LOCALIZACIÓN
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        A través del análisis de Big Data se definen las ubicaciones que cumplan los requisitos de inversión de u&v. Reforzamos nuestra propuesta de valor a traves herramientas de Big Data.
+        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <i class="fas fa-search tab-icon"></i> ANÁLISIS
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        Proceso exhaustivo de análisis comercial, financiero y técnico. A través de modelos de Machine Learning se procesan millones de datos tanto internos como externos que permiten fácilmente identificar los insights más valiosos.
+        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <i class="fas fa-cog tab-icon"></i> ESTRUCTURACIÓN
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        Captación de fondos para financiar la adquisición y desarrollos de los proyectos. La estructura buscada es la que combine el capital privado / institucional y financiación bancaria. u&v cuenta con una plataforma digital que permite la eficiente gestión de los activos y transparencia para los inversores
+        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFour">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          <i class="fas fa-file-signature tab-icon"></i> GESTIÓN
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+      <div class="card-body">
+        Estructura de desarrollo óptima a través de contratos de gestión con fees predefinidos. Estándares LEED, proyectos en base BIM y monitorización a través de la plataforma de inversión.
+        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFive">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/logo-urban-value-tab.png" class="tab-icon"> RETORNOS
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+      <div class="card-body">
+         La suma de los elementos anteriormente descritos, resulta en la concesión de los objetivos previamente acordados respecto a la inversión, tanto económicos como de plazo
+         <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+      </div>
+    </div>
+  </div>
+</div>
+			
+			
+		</div>
+		</div>
+	</div>
+</section>
+
 <!-- claim -->
 <section class="claim d-md-block d-lg-flex h-100 pb-md-5" >
 	<div class="container justify-content-lg-center align-self-lg-center">
