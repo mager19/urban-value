d940ae3a653bf840e6b11b4a5191621f7111318d
Avraxas <avraxas@gmail.com>
2018-11-29 07:19:50 -0500

    ajustes responsive

--- page-home.php
+++ page-home.php
@@ -33,7 +33,7 @@ get_header();?>
 				<h3 class="mb-3"><b>Una propuesta de valor única en el sector.</b></h3>
 			</div>
 		</div>
-		<div class="row mt-5">
+		<div class="row mt-5 desk-top">
 			<?php
 				if( have_rows('items_bajo_video') ):
 							
@@ -72,19 +72,7 @@ get_header();?>
 				endif;
 			?>
 		</div>
-	</div>
-</section>
-<!-- desarrollo -->
-<section class="desarrollo d-md-block d-lg-flex h-100 pb-md-5" >
-	<div class="container justify-content-lg-center align-self-lg-center">
-		<div class="row ">
-			<div class="col-lg-12 text-center">
-				<h2 class="mb-3"><b>U&V DESARROLLA PROYECTOS INMOBILIARIOS INNOVADORES</b></h2>
-				<h4 class="mb-3">Creamos valos a nuestros inversores a través de una gestión efectiva, fuerte y sostenible con una base tecnológica integrada.</h4>
-				<h3 class="mb-3"><b>Una propuesta de valor única en el sector.</b></h3>
-			</div>
-		</div>
-		
+		<!-- movil -->
 		<div class="row mt-5 dess">
 			<?php
 				if( have_rows('items_bajo_video') ):
@@ -126,6 +114,7 @@ get_header();?>
 		</div>
 	</div>
 </section>
+
 <!-- tabbs -->
 <section class="tabbs d-md-block d-lg-flex h-100 pb-md-5" >
 	<div class="container justify-content-lg-center align-self-lg-center">
@@ -137,7 +126,7 @@ get_header();?>
 				
 			</div>
 		</div>
-		<div class="row mt-5">
+		<div class="row mt-5 desk-top">
 			<div class="row">
 			<div class="nav flex-column nav-pills col-lg-3 offset-lg-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 			  <a class="nav-link active tt1" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
@@ -170,108 +159,98 @@ get_header();?>
 			</div>
 		</div>
 		</div>
-	</div>
-</section>
+		<!-- movil -->
 
-
-<!-- tabbs2 -->
-<section class="tabbs2 d-md-block d-lg-flex h-100 pb-md-5" >
-	<div class="container justify-content-lg-center align-self-lg-center">
-		<div class="row ">
-			<div class="col-lg-12 text-center">
-				<h2 class="mb-3"><b>PROCESO DE INVERSIÓN</b></h2>
-				<h4 class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				</h4>
-				
-			</div>
-		</div>
-		<div class="row mt-5">
+		<div class="row mt-5 mov-il">
 			<div class="row">
 
-<div id="accordion">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-         <i class="fas fa-map-marker-alt tab-icon"></i> LOCALIZACIÓN
-        </button>
-      </h5>
-    </div>
+			<div id="accordion">
+			  <div class="card">
+			    <div class="card-header" id="headingOne">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			         <i class="fas fa-map-marker-alt tab-icon"></i> LOCALIZACIÓN
+			        </button>
+			      </h5>
+			    </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div class="card-body">
-        A través del análisis de Big Data se definen las ubicaciones que cumplan los requisitos de inversión de u&v. Reforzamos nuestra propuesta de valor a traves herramientas de Big Data.
-        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          <i class="fas fa-search tab-icon"></i> ANÁLISIS
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div class="card-body">
-        Proceso exhaustivo de análisis comercial, financiero y técnico. A través de modelos de Machine Learning se procesan millones de datos tanto internos como externos que permiten fácilmente identificar los insights más valiosos.
-        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          <i class="fas fa-cog tab-icon"></i> ESTRUCTURACIÓN
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div class="card-body">
-        Captación de fondos para financiar la adquisición y desarrollos de los proyectos. La estructura buscada es la que combine el capital privado / institucional y financiación bancaria. u&v cuenta con una plataforma digital que permite la eficiente gestión de los activos y transparencia para los inversores
-        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingFour">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-          <i class="fas fa-file-signature tab-icon"></i> GESTIÓN
-        </button>
-      </h5>
-    </div>
-    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
-      <div class="card-body">
-        Estructura de desarrollo óptima a través de contratos de gestión con fees predefinidos. Estándares LEED, proyectos en base BIM y monitorización a través de la plataforma de inversión.
-        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingFive">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/logo-urban-value-tab.png" class="tab-icon"> RETORNOS
-        </button>
-      </h5>
-    </div>
-    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
-      <div class="card-body">
-         La suma de los elementos anteriormente descritos, resulta en la concesión de los objetivos previamente acordados respecto a la inversión, tanto económicos como de plazo
-         <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
-      </div>
-    </div>
-  </div>
-</div>
-			
+			    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+			      <div class="card-body">
+			        A través del análisis de Big Data se definen las ubicaciones que cumplan los requisitos de inversión de u&v. Reforzamos nuestra propuesta de valor a traves herramientas de Big Data.
+			        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingTwo">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			          <i class="fas fa-search tab-icon"></i> ANÁLISIS
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+			      <div class="card-body">
+			        Proceso exhaustivo de análisis comercial, financiero y técnico. A través de modelos de Machine Learning se procesan millones de datos tanto internos como externos que permiten fácilmente identificar los insights más valiosos.
+			        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingThree">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			          <i class="fas fa-cog tab-icon"></i> ESTRUCTURACIÓN
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			      <div class="card-body">
+			        Captación de fondos para financiar la adquisición y desarrollos de los proyectos. La estructura buscada es la que combine el capital privado / institucional y financiación bancaria. u&v cuenta con una plataforma digital que permite la eficiente gestión de los activos y transparencia para los inversores
+			        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingFour">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+			          <i class="fas fa-file-signature tab-icon"></i> GESTIÓN
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+			      <div class="card-body">
+			        Estructura de desarrollo óptima a través de contratos de gestión con fees predefinidos. Estándares LEED, proyectos en base BIM y monitorización a través de la plataforma de inversión.
+			        <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingFive">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+			          <img src="<?php echo get_template_directory_uri(); ?>/img/logo-urban-value-tab.png" class="tab-icon"> RETORNOS
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+			      <div class="card-body">
+			         La suma de los elementos anteriormente descritos, resulta en la concesión de los objetivos previamente acordados respecto a la inversión, tanto económicos como de plazo
+			         <div class="text-center techs"><img src="https://placeimg.com/60/60/tech" class="pr-2"><img src="https://placeimg.com/60/60/tech"><img src="https://placeimg.com/60/60/tech" class="pl-2"></div>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+						
 			
 		</div>
 		</div>
+
 	</div>
 </section>
+
+
 
 <!-- Filosofia -->
 <section class="filosofia d-md-block d-lg-flex flex-lg-column h-100 pb-md-5">
@@ -322,8 +301,12 @@ get_header();?>
 		
 	</div>
 
+
+
+</section>
+
 <!-- Impacto -->
-<div class="impacto d-lg-flex flex-lg-column mt-5">
+<section class="impacto d-lg-flex flex-lg-column">
 	<div class="justify-content-lg-center align-self-lg-center items">
 		<div class="row ">
 			<div class="col-lg-12">
@@ -349,8 +332,6 @@ get_header();?>
                 	wp_reset_postdata();?>
 		</div>
 	</div>
-</div>
-
 </section>
 
 <!-- Proyectos -->
